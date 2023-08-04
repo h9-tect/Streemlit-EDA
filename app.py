bebcd02dd23d37b30c6f55804c5b0b1911f7f0ba
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-08-04 08:24:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 import streamlit as st
+import pandas as pd
+import pandas_profiling as pp
 from mitosheet.streamlit.v1 import spreadsheet
+from streamlit_pandas_profiling import st_profile_report
 
+# Set page layout
 st.set_page_config(layout="wide")
-st.title('Tesla Stock Volume Analysis')
 
-CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
-new_dfs, code = spreadsheet(CSV_URL)
+# Set page title
+st.title('Mito EDA')
 
-st.write(new_dfs)
-st.code(code)
+# Get CSV URL from user input
+CSV_URL = st.text_input('Enter CSV URL')
+
+# Load CSV data
+try:
+    data = pd.read_csv(CSV_URL)
+except:
+    st.warning('Invalid CSV URL. Please enter a valid CSV URL.')
+    st.stop()
+
+    # Perform EDA using pandas_profiling and Mito Spreadsheet
+    report = pp.ProfileReport(data)
+    new_dfs, code = spreadsheet(report.to_widgets())
+
+    # Display EDA report
+    st_profile_report(new_dfs)
+    st.code(code)
+else:
+    st.warning('Please enter a CSV URL.')
